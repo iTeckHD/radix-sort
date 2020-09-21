231ac69086dbf4a507ac03259e0761ba1fad1ccc
Tuan Kha Tran <ituankha.tran@gmail.com>
2020-09-21 09:13:16 +0200

    rm: dist

--- dist/get-number-of-digits.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getNumberOfDigits = void 0;
-// source: https://stackoverflow.com/a/28203456
-function getNumberOfDigits(x) {
-    return Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
-}
-exports.getNumberOfDigits = getNumberOfDigits;
